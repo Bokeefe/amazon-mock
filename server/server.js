713101a7680f34c465cfb445ad12fbd8c3f4dd23
Brendan O'Keefe <brndnokf@yahoo.com>
2020-04-14 08:22:31 -0600

    mongo connected local, env var

--- server/server.js
+++ server/server.js
@@ -1,27 +1,60 @@
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const path = require("path");
+dotenv.config({
+  path: path.join(__dirname, ".env"),
+});
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
 const port = 3000;
 
+// models
+const User = require("./models/user");
+
+mongoose.connect(
+  process.env.DATABASE,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, db) => {
+    if (err) throw err;
+  }
+);
+
 const app = express();
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(morgan("dev"));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
-app.get('/', (req, res) => {
-    res.json('hello there');
-})
+app.get("/", (req, res) => {
+  res.json("hello there");
+});
 
-app.post('/', (req, res) => {
-    console.log(req.body.name)
-    res.json(`your name is ` + req.body.name)
-})
+app.post("/", (req, res) => {
+  let user = new User();
+  user.name = req.body.name;
+  user.email = req.body.email;
+  user.password = req.body.password;
 
-app.listen(port, err => {
+  user.save((err) => {
     if (err) {
-        console.log(err);
+      res.send(err);
     } else {
-        console.log('listening on port:', port);
+      res.json("successful user save");
     }
+  });
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("listening on port:", port);
+  }
 });
